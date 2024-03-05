a47822818c3688a0d4de2dc90b02d98eb5b597d0
dorfbl@gmail.com <dorfbl@gmail.com>
2024-03-05 11:11:03 +0000

    For new docker image

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -9,26 +9,23 @@ export class ProductsController {
   constructor(private productsService: ProductsService) {}
 
   @Get()
-  getall(): string {
-    return 'Hi';
-  }
   // @hasRoles(UserRole.ADMIN)
   // @UseGuards(JwtAuthGuard, RolesGuard)
-  // index(
-  //   @Query('page') page: number = 1,
-  //   @Query('limit') limit: number = 30,
-  //   @Query('orderBy') orderBy?: string,
-  //   @Query('sortOrder') sortOrder: 'ASC' | 'DESC' = 'ASC',
-  // ): Observable<Pagination<Product>> {
-  //   limit = limit > 100 ? 100 : limit;
-  //   return this.productsService.paginate(
-  //     {
-  //       page: +page,
-  //       limit: +limit,
-  //       route: 'http://192.168.0.10:3000/products',
-  //     },
-  //     orderBy,
-  //     sortOrder,
-  //   );
-  // }
+  index(
+    @Query('page') page: number = 1,
+    @Query('limit') limit: number = 30,
+    @Query('orderBy') orderBy?: string,
+    @Query('sortOrder') sortOrder: 'ASC' | 'DESC' = 'ASC',
+  ): Observable<Pagination<Product>> {
+    limit = limit > 100 ? 100 : limit;
+    return this.productsService.paginate(
+      {
+        page: +page,
+        limit: +limit,
+        route: 'http://192.168.0.10:3000/products',
+      },
+      orderBy,
+      sortOrder,
+    );
+  }
 }
